597e3e06f6c012052135e38f4d85cd6b99b92125
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-11-13 00:34:47 +0100

    Modification de la classe Plateau

--- sources/modele/src/main/java/objets/Plateau.java
+++ sources/modele/src/main/java/objets/Plateau.java
@@ -1,8 +1,32 @@
 package objets;
 
+import cartes.CartePropagation;
+import cartes.CartesJoueurs;
+import cartes.DefausseJoueur;
+import cartes.DefaussePropagation;
+import eclosions.PisteDeclosion;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Plateau {
-    public List<Ville> LesVilles;
+    private List<Ville> LesVilles;
+    private List<CartesJoueurs> cartesJoueurs;
+    private List<CartePropagation> cartesPropagation;
+    private List<DefausseJoueur> cartesDeLaDefausseJoueur;
+    private List<DefaussePropagation> cartesDeLaDefaussePropagation;
+    private PisteDeclosion pisteDeclosion;
+    //private int[] pisteDeclosion ;
+    private int[] pisteDePropagation;
 
+    public Plateau(List<Ville> lesVilles, List<CartesJoueurs> cartesJoueurs, List<CartePropagation> cartesPropagation, List<DefausseJoueur> cartesDeLaDefausseJoueur, List<DefaussePropagation> cartesDeLaDefaussePropagation, int[] pisteDeclosion, int[] pisteDePropagation) {
+        LesVilles = new ArrayList<>();
+        this.cartesJoueurs = new ArrayList<>();
+        this.cartesPropagation = new ArrayList<>();
+        this.cartesDeLaDefausseJoueur = new ArrayList<>();
+        this.cartesDeLaDefaussePropagation = new ArrayList<>();
+        this.pisteDeclosion = this.pisteDeclosion;
+        //this.pisteDeclosion = new int  [8];
+        this.pisteDePropagation = pisteDePropagation;
+    }
 }
