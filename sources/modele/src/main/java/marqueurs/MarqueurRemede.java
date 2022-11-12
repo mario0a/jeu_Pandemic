bee4b67198ab64f9db178a6803a956ca5b71d29a
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-11-12 21:35:43 +0100

    implémentation de la classe MarqueurRemede

--- sources/modele/src/main/java/marqueurs/MarqueurRemede.java
+++ sources/modele/src/main/java/marqueurs/MarqueurRemede.java
@@ -1,4 +1,26 @@
 package marqueurs;
 
 public class MarqueurRemede {
+    public String recto;
+    public String verso;
+
+    public MarqueurRemede(String recto) {
+        this.recto = recto;
+    }
+
+    public String getRecto() {
+        return recto;
+    }
+
+    public void setRecto(String recto) {
+        this.recto = recto;
+    }
+
+    public String getVerso() {
+        return verso;
+    }
+
+    public void setVerso(String verso) {
+        this.verso = verso;
+    }
 }
