527d503b3e49dfb63033204a61a892d16c04a929
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-11-13 01:15:40 +0100

    création de la table EmplacementMaladies pour l'emplacement des marqueurs remède sur le plateau

---
+++ sources/modele/src/main/java/objets/EmplacementMaladies.java
@@ -0,0 +1,19 @@
+package objets;
+
+import marqueurs.MarqueurRemede;
+
+public class EmplacementMaladies {
+    //Couleurs couleurMaladie;
+    private MarqueurRemede marqueurRemede;
+
+    public EmplacementMaladies() {}
+
+    public MarqueurRemede getMarqueurRemede() {
+        return marqueurRemede;
+    }
+
+    public void setMarqueurRemede(MarqueurRemede marqueurRemede) {
+        this.marqueurRemede = marqueurRemede;
+    }
+
+}
