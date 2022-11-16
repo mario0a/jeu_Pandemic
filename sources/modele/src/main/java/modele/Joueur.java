baa849e055533ab72b6d543ffe71ae33c11b2a78
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-11-17 00:33:52 +0100

    création  de la classe Joueur

---
+++ sources/modele/src/main/java/modele/Joueur.java
@@ -0,0 +1,11 @@
+package modele;
+
+import cartes.Carte;
+
+import java.util.List;
+
+public class Joueur {
+    private TypeRole typeRole;
+    private List<Carte> cartesJoueur;
+    private int nombreCarteJoueur;
+}
