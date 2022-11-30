3994af80cb5ea5c3d77305b144887505b18b2b1b
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-11-30 02:58:38 +0100

    Création de la classe ContexteDeplacement.java qui permettra de faire les déplacements

---
+++ sources/modele/src/main/java/LesActions/ContexteDeplacement.java
@@ -0,0 +1,29 @@
+package LesActions;
+
+import exceptions.CarteArriveeInexistanteException;
+import facade.Joueur;
+
+public class ContexteDeplacement {
+    private IDeplacements lesDeplacements;
+
+    public ContexteDeplacement(IDeplacements lesDeplacements) {
+        this.lesDeplacements = lesDeplacements;
+    }
+
+    public void setLesDeplacements(IDeplacements lesDeplacements) {
+        this.lesDeplacements = lesDeplacements;
+    }
+
+    public IDeplacements getLesDeplacements() {
+        return lesDeplacements;
+    }
+
+    //délègue le déplacement d'un pion à la stratégie lesDeplacements qui fera à la bonne stratégie concrète de déplacement
+    public void executerDeplacement(Joueur joueur){
+        try {
+            lesDeplacements.executeDeplacement(joueur); // lève une exception, à traiter!!
+        } catch (CarteArriveeInexistanteException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
