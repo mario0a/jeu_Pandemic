ad7b82eb1b2a5d145d615ff1f18f19b28b06eff1
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-11-30 02:26:19 +0100

    ajout de la méthode execute dans l'interface Deplacement (début d'utilisation du pattern stratégie)

--- sources/modele/src/main/java/LesActions/Deplacement.java
+++ sources/modele/src/main/java/LesActions/Deplacement.java
@@ -1,4 +1,5 @@
 package LesActions;
 //mettre en place le pattern stratégie pour gérer les déplacments
 public interface Deplacement {
+    public void executeData(); // permettra au context d'exécuter les différentes stratégies de déplacement
 }
