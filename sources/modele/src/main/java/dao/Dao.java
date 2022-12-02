a7db9c5b4de68f706e51308b7f5a096a28bb642b
mario0a <78761637+mario0a@users.noreply.github.com>
2022-12-02 22:42:22 +0100

    creation de la Dao pour la base de donnée ainsi que le confifiration des dependances mongoDB.

---
+++ sources/modele/src/main/java/dao/Dao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+public class Dao {
+    private static final MongoClient mongoClient = MongoClients.create("mongodb://172.17.0.2:27017");
+    private static final CodecRegistry pojoCodeRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+    private static final MongoDatabase db = mongoClient.getDatabase("pandemic").withCodecRegistry(pojoCodeRegistry);
+
+
+
+
+
+}
