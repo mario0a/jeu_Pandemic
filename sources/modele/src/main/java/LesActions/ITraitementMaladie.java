3a8a53439c2240511cd4a68fcb4742c2201121bc
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-17 13:49:46 +0100

    Création de l'interface ITraitementMaladie.java

---
+++ sources/modele/src/main/java/LesActions/ITraitementMaladie.java
@@ -0,0 +1,8 @@
+package LesActions;
+
+import modele.CouleursMaladie;
+import modele.Joueur;
+
+public interface ITraitementMaladie {
+    void traiterMaladie(Joueur joueur, CouleursMaladie couleurMaladie);
+}
