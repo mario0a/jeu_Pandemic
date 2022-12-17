53a5a1c616b84cae9897445ade3bf584a16a3f4c
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-17 11:54:13 +0100

    Création de la classe PiocherCartes.java

--- sources/modele/src/main/java/LesActions/PiocherCartes.java
+++ sources/modele/src/main/java/LesActions/PiocherCartes.java
@@ -7,6 +7,14 @@ import modele.Joueur;
 
 import java.util.List;
 
-public interface PiocherCartes {
-    void piocherCarte(Joueur joueur, List<CartesJoueur> cartesJoueurList) throws CartesJoueurInsuffisantes, NombreCarteDepasseException;
+public class PiocherCartes implements IPiocherCartes{
+    @Override
+    public void piocherCarte(Joueur joueur, List<CartesJoueur> cartesJoueurList) throws CartesJoueurInsuffisantes, NombreCarteDepasseException {
+        if(cartesJoueurList.size()<2) throw new CartesJoueurInsuffisantes(); // activer fin de partie
+        if(joueur.getCartes_en_main().size()>5) throw new NombreCarteDepasseException();// ajouter l'option défosser carte
+        joueur.getCartes_en_main().add(cartesJoueurList.get(cartesJoueurList.size()-1));
+        cartesJoueurList.remove(cartesJoueurList.size()-1);
+        joueur.getCartes_en_main().add(cartesJoueurList.get(cartesJoueurList.size()-1));
+        cartesJoueurList.remove(cartesJoueurList.size()-1);
+    }
 }
