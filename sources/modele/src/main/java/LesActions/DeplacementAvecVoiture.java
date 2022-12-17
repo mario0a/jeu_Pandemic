452f6a1079a34710d0dac64884c2cb12c3f3f650
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-17 01:18:59 +0100

    Modification de la classe DeplacementAvecVoiture.java

--- sources/modele/src/main/java/LesActions/DeplacementAvecVoiture.java
+++ sources/modele/src/main/java/LesActions/DeplacementAvecVoiture.java
@@ -3,11 +3,22 @@ package LesActions;
 //pertmet de se déplacer d'une ville à une autre à condition qu'il y ait un trait qui réunit ces 2 destinations
 
 import exceptions.CarteArriveeInexistanteException;
+import exceptions.PasCentreRechercheException;
 import modele.Joueur;
+import modele.Ville;
+
+import java.util.List;
 
 public class DeplacementAvecVoiture implements IDeplacements{
-    @Override
-    public void operationDeplacement(Joueur joueur) throws CarteArriveeInexistanteException {
 
+    @Override
+    public void operationDeplacement(Joueur joueur, Ville choix) throws PasCentreRechercheException {
+
+        List<Ville> villesVoisines=joueur.getPosition().getVillesLiees();
+        if(villesVoisines.contains(choix)){
+            joueur.setPosition(choix);
+        }else{
+            System.out.println("Le joueur n'a pas le droit de se déplacer en voiture, veuillez choisir un autre moyen de déplacement");
+        }
     }
 }
