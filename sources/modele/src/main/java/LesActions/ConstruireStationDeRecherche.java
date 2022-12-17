5216573e98eca00e2c9693b0eb8081be79b52c70
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-17 11:59:46 +0100

    creéation de la classe ConstruireStationDeRecherche.java

--- sources/modele/src/main/java/LesActions/ConstruireStationDeRecherche.java
+++ sources/modele/src/main/java/LesActions/ConstruireStationDeRecherche.java
@@ -6,8 +6,7 @@ import modele.Joueur;
 import modele.Plateau;
 
 public class ConstruireStationDeRecherche {
-    private String villeAUneStationDeRecherche;
-
+    //private String villeAUneStationDeRecherche;
     public void construireStationDeRecherche(Joueur joueur, Plateau plateau) throws CarteArriveeInexistanteException {
         if (joueur.getCartes_en_main().stream().anyMatch(carte->carte.informations().equals(joueur.getPosition().getNomVille()))){
             if (JeuDeCartes.lesVilles().stream().anyMatch(v ->v.getNomVille().equals(joueur.getPosition().getNomVille()))){
