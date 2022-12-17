f8f286668c63852134eb9d9705dbb08400211a1d
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-17 23:53:21 +0100

    Modification de la classe Partie.java

--- sources/modele/src/main/java/modele/Partie.java
+++ sources/modele/src/main/java/modele/Partie.java
@@ -1,4 +1,23 @@
 package modele;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Partie {
+    private String id;
+    private EtatPartie etatPartie;
+    private LocalDate dateCreation;
+    private List<CarteVille> carteDefausse;
+    private List<Partie1Joueur> partieJoueur;
+
+    public Partie() {}
+
+    public Partie(String id) {
+        this.id = id;
+        this.dateCreation = LocalDate.now();
+        this.partieJoueur= new ArrayList<>();
+        this.carteDefausse = new ArrayList<>();
+        this.etatPartie = EtatPartie.DEBUT;
+    }
 }
