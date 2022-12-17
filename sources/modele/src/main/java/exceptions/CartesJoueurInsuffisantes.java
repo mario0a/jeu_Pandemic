fe43623637bd5b772f9af9455a9cab5d6abf629f
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-17 11:57:10 +0100

    Création de nouvelles exceptions

---
+++ sources/modele/src/main/java/exceptions/CartesJoueurInsuffisantes.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class CartesJoueurInsuffisantes extends Exception {
+}
---
+++ sources/modele/src/main/java/exceptions/NombreCarteDepasseException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class NombreCarteDepasseException extends Exception {
+}
