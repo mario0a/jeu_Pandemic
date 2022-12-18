31ae01abab36baea93d5891bcfb72cd16a502d8d
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-18 01:14:03 +0100

    Modification de l'interface IFacadePandemicOnline.java

--- sources/modele/src/main/java/facade/IFacadePandemicOnline.java
+++ sources/modele/src/main/java/facade/IFacadePandemicOnline.java
@@ -3,6 +3,9 @@ package facade;
 import exceptions.PartieNonRepriseException;
 import exceptions.PartiePleineException;
 import modele.Partie;
+import modele.Plateau;
+import modele.Ville;
+import modele.interfaces.ICartes;
 
 import java.util.Collection;
 
@@ -19,4 +22,27 @@ public interface IFacadePandemicOnline {
  public  Collection<Partie> getLesPartiesSuspendues();
  public  boolean suspendreLaPartie(String idPartie, String nomJoueur)throws PartieNonRepriseException;
  public  boolean quitterLaPartie(String idPartie, String nomJoueur);
+ public  boolean peutQuitterLaPartie(String idPartie);
+ public  boolean createurPartie(String idPartie, String nomJoueur);
+ //permet au joueur de piocher des cartes dans mes différentes piles de cartes
+ public void piocherCarte(ICartes carte);
+
+ public void seDeplacer();
+ public void contruireUneStationDeRcherche();
+ //permet de traiter une maladie dans la ville où on se trouve
+ public void traiterUneMaladie();
+ //permet de partager des connaissances avec un joueur en lui donnant ou en prenant la carte ville dans laquelle on se trouve
+ public void partagerDesConnaissances();
+ public void trouvrerRemede();
+ public boolean carte_en_main_par_rapport_a_position(Ville ville);
+ public void diminuerActions();
+ public boolean carte_en_main();
+ public void diminuer_carte_en_main();
+ public void distribution(String idPartie);
+ void partieCommence(String idPartie);
+ void accederPartie(String idPartie, String pseudo);
+ void accederUnePartie(String idPartie, String pseudo);
+ boolean connexion(String pseudo, String mdp);
+ boolean reAccederAuJeu(String idPartie,String pseudo);
+ Collection<Plateau> getLesPartiesSuspendues(String idPArtie, String pseudo);
 }
