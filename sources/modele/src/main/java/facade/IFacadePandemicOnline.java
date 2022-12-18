f5a6362e6feb94968fd112803d4897b3c9e16b1f
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2022-12-18 02:29:41 +0100

    interface IFacadePandemicOnline.java terminée

--- sources/modele/src/main/java/facade/IFacadePandemicOnline.java
+++ sources/modele/src/main/java/facade/IFacadePandemicOnline.java
@@ -1,6 +1,7 @@
 package facade;
 
 import exceptions.PartieNonRepriseException;
+import exceptions.PartieNonSuspenduException;
 import exceptions.PartiePleineException;
 import modele.Partie;
 import modele.Plateau;
@@ -14,35 +15,25 @@ public interface IFacadePandemicOnline {
  //contient les différentes actions qu'on peut effectuer sur le plateau
  public boolean partieInitialisee(String idPartie);
 
- public  void inscription(String nomJoueur, String mdp);
- public  boolean seConnecter(String nomJoueur, String mdp);
- public  void creerPartie(String id, String nomJoueur) throws PartiePleineException;
- public  Collection<Partie> getLesParties();
- public  String getEtatPartie(String id);
- public  Collection<Partie> getLesPartiesSuspendues();
- public  boolean suspendreLaPartie(String idPartie, String nomJoueur)throws PartieNonRepriseException;
- public  boolean quitterLaPartie(String idPartie, String nomJoueur);
- public  boolean peutQuitterLaPartie(String idPartie);
- public  boolean createurPartie(String idPartie, String nomJoueur);
- //permet au joueur de piocher des cartes dans mes différentes piles de cartes
- public void piocherCarte(ICartes carte);
+ public void inscription(String nomJoueur, String mdp);
 
- public void seDeplacer();
- public void contruireUneStationDeRcherche();
- //permet de traiter une maladie dans la ville où on se trouve
- public void traiterUneMaladie();
- //permet de partager des connaissances avec un joueur en lui donnant ou en prenant la carte ville dans laquelle on se trouve
- public void partagerDesConnaissances();
- public void trouvrerRemede();
- public boolean carte_en_main_par_rapport_a_position(Ville ville);
- public void diminuerActions();
- public boolean carte_en_main();
- public void diminuer_carte_en_main();
- public void distribution(String idPartie);
- void partieCommence(String idPartie);
- void accederPartie(String idPartie, String pseudo);
- void accederUnePartie(String idPartie, String pseudo);
- boolean connexion(String pseudo, String mdp);
- boolean reAccederAuJeu(String idPartie,String pseudo);
- Collection<Plateau> getLesPartiesSuspendues(String idPArtie, String pseudo);
+ public boolean seConnecter(String nomJoueur, String mdp);
+
+ public void creerPartie(String id, String nomJoueur) throws PartiePleineException;
+
+ public Collection<Partie> getLesParties();
+
+ public String getEtatPartie(String id);
+
+ public Collection<Partie> getLesPartiesSuspendues();
+
+ public boolean suspendreLaPartie(String idPartie, String nomJoueur) throws PartieNonRepriseException;
+
+ public boolean quitterLaPartie(String idPartie, String nomJoueur);
+
+ public boolean reprendreUnePartie(String idPartie, String nomJoueur) throws PartieNonSuspenduException;
+
+ public boolean peutQuitterLaPartie(String idPartie);
+
+ public boolean createurPartie(String idPartie, String nomJoueur);
 }
